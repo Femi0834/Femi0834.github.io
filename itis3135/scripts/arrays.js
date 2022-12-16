cd460b231d6d312533c2471fe3a910649880a7e6
Obafemi Oshin <Femi0834@gmail.com>
2022-12-15 23:34:11 -0500

    lknknljnlk

--- itis3135/scripts/arrays.js
+++ itis3135/scripts/arrays.js
@@ -1,32 +1,60 @@
-var days = ["Monday", "Tuesday"];
-var temperature = [80,70];
-
-var $ = function (id) { return document.getElementById(id); };
-
-
-window.onload = function () {
-
-    $("display_results").onclick = displayResults;
-    $("add_temperature").onclick = addTemperature;
-    $("display_temperature").onclick = displayTemperature;
-    $("days").focus();
-    
+window.onload = function() {
+    document.getElementById("add_salary").onclick = addSalary;
+    document.getElementById("display_results").onclick = displayResults;
+    document.getElementById("display_salary").onclick = displaySalary;
+    document.getElementById("person").focus;
 };
 
-function addTemperature(){
-    var dayInput = document.getElementById("days").value;
-    var temperatureInput = document.getElementById("temperature").value;
+var person = [];
+var salary = [];
+
+function addSalary() {
+    var enteredSalary = parseFloat(document.getElementById("salary").value);
+    var enteredName = document.getElementById("person").value;
+
+    enteredSalary = parseFloat(enteredSalary);
+    person.push(enteredName);
+    salary.push(enteredSalary);
+
+    document.getElementById("salary").value = "";
+    document.getElementById("person").focus();
+}
+
+function displayResults() {
+    var average = 0;
+    var sum = 0;
+    var personWithHighestSalary = "";
+    var maxNumber = 0;
+
+    for(i = 0; i < salary.length; i++)
+    {
+        sum += salary[i];
+    }
     
-   if(temperatureInput>150 || temperatureInput<-30 || temperatureInput == ""){
-       alert("You must enter a day and a valid temperature");
-       temperatureInput = "";
-       $("days").focus();
-   }
-   else {
-       temperatureInput = parseInt(temperatureInput);
-       days.push(dayInput);
-       temperature.push(temperatureInput);
-       $("temperature").innerHTML = "" ;
-       $("days").focus();
-   }
+    average = (sum / salary.length);
+
+    for(i = 0; i < salary.length; i++)
+    {
+        if(salary[i] > maxNumber)
+        {
+            maxNumber = salary[i];
+            personWithHighestSalary = person[i];
+        }
+    }
+
+    document.getElementById("results").innerHTML = "The average salary is $" + average.toFixed(2) + ". " + personWithHighestSalary + " had the highest salary at $" + maxNumber.toFixed(2);
+}
+
+function displaySalary() {
+    var table;
+    table = "<table><tr><th>Employee</th><th>Salary</th></tr>"
+
+    for(i = 0; i < salary.length; i++)
+    {
+        table += "<tr><td>" + person[i] + "</td><td>" + salary[i] + "</td></tr>";
+    }
+
+    table += "</table>";
+
+    document.getElementById("table_results").innerHTML = table;
 }
